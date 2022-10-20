cc75459e74c51ba9d726f4fe189b7e429dd8389e
Junbeeom <wns_p@naver.com>
2022-10-20 21:17:36 +0900

    init

---
+++ src/main/java/com/example/RestFulApi/ChoJunBeomApplication.java
@@ -0,0 +1,13 @@
+package com.example.RestFulApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoJunBeomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChoJunBeomApplication.class, args);
+	}
+
+}
